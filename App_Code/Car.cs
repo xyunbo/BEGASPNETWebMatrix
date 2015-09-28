ef01a63a415806dcc596c46d7dffb27954aa89d8
xyunbo <xyunbo@yahoo.com>
2015-09-27 23:54:03 -0400

    finish chapter 13

---
+++ App_Code/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassName
+/// </summary>
+public class Car
+{
+    public string Model { get; set; }
+    public int NumberOfDoors { get; set; }
+    public int YearRegistered { get; set; }
+    public string Color { get; set; }
+}
